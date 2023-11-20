853ddc4e0d51e4c9d2dfe3f35f172fb5f2ef6310
Lucas Nolte <lucas@designsystems.international>
2023-11-20 20:22:19 +0100

    test: more granular useParamState tests

--- packages/react-hooks/tests/useParamState.test.js
+++ packages/react-hooks/tests/useParamState.test.js
@@ -13,6 +13,7 @@ describe('useParamState', () => {
   });
 
   it('should update the URL search string when a value is updated', () => {
+    console.log(window.location.search);
     const paramName = 'count';
     const initialValue = 10;
     const { result } = renderHook(() => useParamState(paramName, initialValue));
@@ -23,11 +24,45 @@ describe('useParamState', () => {
     expect(result.current[0]).toBe(20);
   });
 
+  it('should allow storing multiple keys in the URL', () => {
+    const firstParam = 'count';
+    const firstValue = 10;
+
+    const secondParam = 'name';
+    const secondValue = 'John';
+
+    const { result } = renderHook(() => {
+      return [useParamState(firstParam, 5), useParamState(secondParam, '')];
+    });
+
+    act(() => {
+      result.current[0][1](firstValue);
+      result.current[1][1](secondValue);
+    });
+
+    expect(window.location.search).toBe(
+      `?${firstParam}=${firstValue}&${secondParam}=${secondValue}`
+    );
+  });
+
   it('should pick up the state from the URL', () => {
-    const paramName = 'count';
-    const initialValue = 10;
-    window.history.pushState({}, '', `?${paramName}=20`);
-    const { result } = renderHook(() => useParamState(paramName, initialValue));
-    expect(result.current[0]).toBe(20);
+    const firstParam = 'count';
+    const firstValue = 10;
+
+    const secondParam = 'name';
+    const secondValue = 'John';
+
+    window.history.pushState(
+      {},
+      '',
+      `?${firstParam}=${firstValue}&${secondParam}=${secondValue}`
+    );
+    const { result } = renderHook(() => [
+      useParamState(firstParam, 20),
+      useParamState(secondParam, ''),
+    ]);
+
+    expect(result.current[0][0]).toBe(firstValue);
+    expect(result.current[1][0]).toBe(secondValue);
   });
 });
